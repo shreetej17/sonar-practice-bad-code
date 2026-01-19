a9e3e390c4314f5e1f40fec2d5246e6bafd14964
shreetej17 <shreetej17@gmail.com>
2026-01-19 22:22:05 +0530

    Add unit test for Calculator's add method

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2, 3));
+    }
+}
