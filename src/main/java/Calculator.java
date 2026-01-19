f989f7ce360fbc76d827431b2106b341d6c285e3
shreetej17 <shreetej17@gmail.com>
2026-01-19 22:20:24 +0530

    add calculator with code smells

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        if (a > 0 && b > 0) {
+            return a + b;
+        } else {
+            return a + b;
+        }
+    }
+
+    public int sum(int x, int y) {
+        if (x > 0 && y > 0) {
+            return x + y;
+        } else {
+            return x + y;
+        }
+    }
+
+    public int multiply(int a, int b) {
+        int unused = 10;
+        return a * b;
+    }
+
+    public boolean isPositive(int a) {
+        if (a > 0) {
+            return true;
+        } else {
+            return true;
+        }
+    }
+}
